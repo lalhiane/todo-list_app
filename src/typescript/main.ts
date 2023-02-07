e542106e701a8a18d50ad8e8602440c34cfd4aa9
unknown <lalhiane1998@gmail.com.com>
2023-02-07 21:45:00 +0100

    TypeScript

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -2,95 +2,115 @@ const todoForm = document.getElementById("todo-form") as HTMLFormElement;
 const todosEl = document.querySelector(".todos") as HTMLDivElement;
 const todoInput = document.querySelector(".todoInput") as HTMLInputElement;
 const todosInfoEl = document.querySelector(".todos-info") as HTMLHeadingElement;
-const clearBtn = document.querySelector(".clear-btn") as HTMLButtonElement;
+const clearBtn = document.querySelector<HTMLButtonElement>(".clear-btn");
 
-interface TodoInterface {
+type Task = {
     id: number;
     title: string;
-    create_new_todo?(id: number, title: string): void;
-    add_todo_to_ls?(todo: any): void;
-    get_todos_from_ls?(): any[];
-    show_all_todos?(): void;
+    completed: boolean;
 }
 
-abstract class Ls implements TodoInterface {
-    constructor (public id: number, public title: string) {}
-    add_todo_to_ls(): void {
-        let todos = this.get_todos_from_ls();
-        localStorage.setItem(
-            "todo",
-            JSON.stringify([...todos, {id: this.id, title: this.title}])
-        )
-    }
-    remove_todo_from_ls(id: number): void {
-        let todos = this.get_todos_from_ls();
-        localStorage.setItem("todo", JSON.stringify(
-            todos.filter(todo => todo.id !== id)
-        ));
-    }
-    get_todos_from_ls(): ({id: number, title: string})[] {
-        let todos =  JSON.parse(localStorage.getItem("todo") as string);
-        return localStorage.getItem("todo") === null ? [] : todos;
-    }
-}
-
-class Ui extends Ls implements TodoInterface {
-    constructor (id: number, title: string) {
-        super(id, title);
-    }
-    create_new_todo(id: number, title: string): void {
-        let todoEl = document.createElement("div");
-        todoEl.className = "todo";
-
-        let todoTitleEl = document.createElement("h3");
-        todoTitleEl.className = "todo-title";
-        todoTitleEl.innerText = title;
-        todoEl.appendChild(todoTitleEl);
-
-        let deleteBtn = document.createElement("button");
-        deleteBtn.className = "fa-solid fa-trash delete-btn";
-        todoEl.appendChild(deleteBtn);
-
-        deleteBtn.addEventListener("click", () => {
-            this.remove_todo_from_ls(id);
-            this.show_all_todos();
-            this.get_todos_info();
-        });
-
-        todosEl.appendChild(todoEl);
-    }
-    show_all_todos(): void {
-        todosEl.innerHTML = "";
-        let todos = this.get_todos_from_ls();
-        todos.forEach(todo => this.create_new_todo(todo.id, todo.title));
-    }
-    fixedInput(): void {
-        todoInput.value = "";
-        todoInput.focus();
-    }
-    get_todos_info():void {
-        let todosLength: number = this.get_todos_from_ls().length;
-        todosInfoEl.innerHTML = `You have ${todosLength} pendding tasks`;
-    }
-}
-
-let generalUiTodo = new Ui(Math.random(), todoInput.value);
-
-generalUiTodo.get_todos_info();
-
-generalUiTodo.show_all_todos();
-
-todoForm.addEventListener("submit", function (e) {
+todoForm.addEventListener("submit", e => {
     e.preventDefault();
-    let newUiTodo = new Ui(Math.random(), todoInput.value);
-    newUiTodo.add_todo_to_ls();
-    newUiTodo.show_all_todos();
-    newUiTodo.fixedInput();
-    newUiTodo.get_todos_info();
+
+    if (todoInput?.value == "" || todoInput?.value == null) return;
+
+    let newTask: Task = {
+        id: Math.random(),
+        title: todoInput.value,
+        completed: false,
+    }
+
+    add_todo_to_ls(newTask);
+
+    show_all_todos();
+
+    fixedInput();
 });
 
-clearBtn.addEventListener("click", function () {
+clearBtn?.addEventListener("click", function () {
     localStorage.clear();
-    generalUiTodo.show_all_todos();
-    generalUiTodo.get_todos_info();
+    show_all_todos();
+    get_todos_info();
 });
+
+function show_all_todos() {
+    todosEl.innerHTML = "";
+    let tasks = get_todos_from_ls();
+    tasks.forEach(task => create_new_todo(task));
+    get_todos_info();
+}
+
+show_all_todos();
+
+function create_new_todo(task: Task): void {
+    let todoEl = document.createElement("div");
+    todoEl.className = "todo";
+
+    let label = document.createElement("label");
+    label.className = "label";
+    label.setAttribute("for", task.title);
+
+    let checkInput = document.createElement("input");
+    checkInput.type = "checkbox";
+    checkInput.id = task.title;
+    checkInput.checked = task.completed;
+    todoEl.appendChild(checkInput);
+
+    checkInput.addEventListener("change", _ => {
+        task.completed = checkInput.checked;
+        add_checked_task_to_ls(task);
+    });
+    
+    label.append(task.title);
+    todoEl.appendChild(label);
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.className = "fa-solid fa-trash delete-btn";
+    todoEl.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click", () => {
+        remove_todo_from_ls(task.id);
+        show_all_todos();
+        get_todos_info();
+    });
+
+    todosEl?.appendChild(todoEl);
+}
+
+function get_todos_info(): void {
+    let tasksLength: number = get_todos_from_ls().length;
+    todosInfoEl.innerHTML = `You have ${tasksLength} tasks`;
+}
+
+function fixedInput() {
+    todoInput.value = "";
+    todoInput.focus();
+}
+
+function add_todo_to_ls(task: Task): void {
+    let tasks = get_todos_from_ls();
+    localStorage.setItem("tasks", JSON.stringify([...tasks, task]));
+}
+
+function remove_todo_from_ls(id: number): void {
+    let tasks = get_todos_from_ls();
+    localStorage.setItem("tasks", JSON.stringify(
+        tasks.filter(task => task.id !== id)
+    ));
+}
+
+function add_checked_task_to_ls(task: Task) {
+    let tasks = get_todos_from_ls();
+
+    tasks.forEach((taskObj) => {
+        if (taskObj.id === task.id) taskObj.completed = task.completed;
+    });
+   
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function get_todos_from_ls(): Task[] {
+    let tasks = JSON.parse(localStorage.getItem("tasks") as string);
+    return localStorage.getItem("tasks") === null ? [] : tasks;
+}
